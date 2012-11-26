c3a69a326909e7f09999dbe4e4c289a6e17cf1cb
root <root@bel-web2.mec.int>
2012-11-26 17:12:55 +0400

    v.1.1

--- reporting_generator.rb
+++ reporting_generator.rb
@@ -1,16 +1,21 @@
 #!/usr/bin/env ruby
+# encoding: utf-8
 # script to get html files from samba share and create html page with list of them
 # (*w)
-$version = "1.0"
+$version = "1.1"
 ##### SETTINGS #####################################################
-site_dir = '/var/www/reporting'
+$site_dir = '/var/www/reporting'
 content_dir = './content'
 $content_owner = 33
 $content_group = 33
 ##### REQUIRE ######################################################
 ##### FUNCTIONS ####################################################
-def cd_to_program_dir!
-	site_dir ? Dir.chdir(site_dir)  : Dir.chdir(File.dirname(__FILE__))
+def get_program_dir!
+	$prog_dir = File.expand_path(File.dirname(__FILE__))
+end
+
+def cd_to_site_dir!
+	$site_dir ? Dir.chdir($site_dir)  : Dir.chdir($prog_dir)
 end
 ##### CLASSES ######################################################
 class Transport
@@ -52,7 +57,7 @@ class Transport
 			raise "Can't access smb share. Install \"smbclient\" first."
 		end
 	end
-
+private
 	def smb_copy_file!(from_d, to_d)
 		puts "\t\tCopy remote file \"#{from_d}\" to local \"#{to_d}\""	
 		`smbclient -W #{@domain} -U #{@user} -c 'get #{from_d} #{to_d}' #{@smb_share} #{@password} 2>/dev/null`
@@ -63,21 +68,23 @@ end
 class Content
 	def initialize(content_dir)
 		@content_dir = content_dir ? content_dir : raise('Content directory not defined')
-		@index_template = './sys/index.html.template'
+		@index_template = "#{$prog_dir}/sys/index.html.template"
 		@index_file = './index.html'
 		@content_owner = $content_owner
 		@content_group = $content_group
+		@title = "Список отчётов"
 	end
 	def build_index!
 		puts "Building \"index.html\" file.."
 		htmls = get_htmls_list(@content_dir)
 		url_list = create_links(htmls)
-		index_content = IO.read(@index_template).sub!('%$index_list$%', url_list)
+		index_content = IO.read(@index_template).gsub!('%$index_list$%', url_list).gsub!('%$title$%', @title)
 		File.open(@index_file,'w'){|file|
 			file.puts(index_content)
 		}
 		File.chown(@content_owner, @content_group, @index_file)
 	end
+private
 	def get_htmls_list(dir)
 		htmls = Array.new
 		Dir.foreach(@content_dir){|entry|
@@ -88,13 +95,15 @@ class Content
 	def create_links(files)
 		url_list = String.new
 		files.each{|file|
-			url_list = "#{url_list}<br><a href=\"#{file}\" target=\"_blank\">#{file}</a>"
+			url_list = "#{url_list}<li><a href=\"#{file}\" target=\"_blank\">#{File.basename(file).gsub!('.html', '')}</a></li>"
+
 		}
-		url_list
+		url_list = "<ul>#{url_list}</ul>"
 	end
 end
 ##### BEGIN ########################################################
-cd_to_program_dir!
+get_program_dir!
+cd_to_site_dir!
 
 transport = Transport.new(content_dir)
 transport.get_new_files!
